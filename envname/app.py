6cac7575b9c3c43319bd58c6bfc4c6e71bfcaaef
Rishi Agarwal <rishiagarwal@172-17-58-108.DYNAPOOL.NYU.EDU>
2017-05-01 20:48:20 -0400

    second last

--- envname/app.py
+++ envname/app.py
@@ -209,6 +209,5 @@ def query_api(term, location):
 
 
 if __name__ == '__main__':
-    # port = int(os.getenv('PORT', 5000))
-    # print("Starting app on port %d" % port)
-    app.run()
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
